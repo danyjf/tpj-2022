eb080c99c37bfc18db4c5a5415bafb891dbab3c8
Daniel Francisco <deny.jo@hotmail.com>
2022-10-10 11:49:46 +0100

    Add collision

--- pygame-hello-world/among_us.py
+++ pygame-hello-world/among_us.py
@@ -3,11 +3,17 @@ from pygame import *
 from pygame.sprite import *
 
 class AmongUs(Sprite):
-    def __init__(self):
+    def __init__(self, location, x, y):
         Sprite.__init__(self)
-        self.image = image.load("among_us.png").convert_alpha()
+        
+        self.image = image.load(location).convert_alpha()
         self.image = pygame.transform.scale(self.image, (170, 200))
-        self.rect = Rect(100, 100, 200, 200)
+        
+        self.rect = Rect(x, y, 150, 150)
+        
+    def update_sprite(self, location):
+        self.image = image.load(location).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (170, 200))
     
     def update(self, vector):
         x, y = vector
--- pygame-hello-world/main.py
+++ pygame-hello-world/main.py
@@ -7,10 +7,12 @@ pygame.init()
 screen = display.set_mode((800, 600))
 display.set_caption("Hello World")
 
-sprite_object = AmongUs()
+sprite_object = AmongUs("among_us_red.png", 100, 100)
+sprite_object2 = AmongUs("among_us_lime.png", 300, 300)
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(sprite_object)
+all_sprites.add(sprite_object2)
 
 clock = pygame.time.Clock()
 
@@ -24,13 +26,21 @@ while True:
         elif e.type == KEYDOWN:
             key = pygame.key.get_pressed()
             if key[K_UP]:
+                undo = (0, 20)
                 sprite_object.update((0, -20))
             if key[K_DOWN]:
+                undo = (0, -20)
                 sprite_object.update((0, 20))
             if key[K_LEFT]:
+                undo = (20, 0)
                 sprite_object.update((-20, 0))
             if key[K_RIGHT]:
+                undo = (-20, 0)
                 sprite_object.update((20, 0))
+    
+    if sprite_object.rect.colliderect(sprite_object2.rect):
+        sprite_object.update(undo)
+        sprite_object2.update_sprite("among_us_lime_dead.png")
     
     screen.fill((0, 0, 0))
     all_sprites.draw(screen)
