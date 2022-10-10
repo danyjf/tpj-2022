80b80c45eb15018ec8a2dc4d047f01554e5eed9c
Daniel Francisco <deny.jo@hotmail.com>
2022-10-10 11:01:26 +0100

    Create hello world in pygame

---
+++ pygame-hello-world/among_us.py
@@ -0,0 +1,13 @@
+﻿import pygame as pygame
+from pygame import *
+from pygame.sprite import *
+
+class AmongUs(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = image.load("among_us.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (170, 200))
+        self.rect = Rect(100, 100, 200, 200)
+    
+    def update(self):
+        pass
---
+++ pygame-hello-world/hello_world.py
@@ -0,0 +1,23 @@
+﻿import pygame as pygame
+from pygame import *
+from pygame.sprite import *
+from among_us import AmongUs
+
+pygame.init()
+screen = display.set_mode((800, 600))
+display.set_caption("Hello World")
+
+hero = AmongUs()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(hero)
+
+while True:
+    e = event.wait()
+    if e.type == QUIT:
+        pygame.quit()
+        break
+    
+    screen.fill((0, 0, 0))
+    all_sprites.draw(screen)
+    display.update()
