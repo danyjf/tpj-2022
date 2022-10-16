ebff1f455f524b33a5c3a3beb65014db7140ed39
Daniel Francisco <deny.jo@hotmail.com>
2022-10-16 20:13:58 +0100

    Refactor code into game class

---
+++ snake/game.py
@@ -0,0 +1,49 @@
+﻿import pygame
+from environment import Environment
+from snake import Snake
+from food import Food
+
+class Game:
+    def __init__(self):
+        self.display = pygame.display.set_mode((Environment.SCALE * Environment.WIDTH, Environment.SCALE * Environment.HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.snake = Snake()
+        self.food = Food()
+    
+    def loop(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.snake.up()
+                    elif event.key == pygame.K_DOWN:
+                        self.snake.down()
+                    elif event.key == pygame.K_LEFT:
+                        self.snake.left()
+                    elif event.key == pygame.K_RIGHT:
+                        self.snake.right()
+                elif event.type == Environment.GAME_EVENT:
+                    if event.name == 'collision':
+                        if event.obj == 'wall' or event.obj == 'snake':
+                            running = False
+                        elif event.obj == 'food':
+                            self.snake.grow()
+                            self.food.randomize_pos()
+
+            # move snake
+            self.snake.move()
+            
+            # snake collisions
+            self.snake.check_collisions(self.food)
+
+            # render
+            self.display.fill("white")
+            
+            self.food.draw(self.display)
+            self.snake.draw(self.display)
+
+            pygame.display.flip()
+            self.clock.tick(15)
--- snake/main.py
+++ snake/main.py
@@ -1,50 +1,13 @@
 ﻿import pygame
-import random
 from food import Food
 from snake import Snake
 from environment import Environment
+from game import Game
 
-display = pygame.display.set_mode((Environment.SCALE * Environment.WIDTH, Environment.SCALE * Environment.HEIGHT))
-clock = pygame.time.Clock()
-
-snake = Snake()
-food = Food()
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                snake.up()
-            elif event.key == pygame.K_DOWN:
-                snake.down()
-            elif event.key == pygame.K_LEFT:
-                snake.left()
-            elif event.key == pygame.K_RIGHT:
-                snake.right()
-        elif event.type == Environment.GAME_EVENT:
-            if event.name == 'collision':
-                if event.obj == 'wall' or event.obj == 'snake':
-                    running = False
-                elif event.obj == 'food':
-                    snake.grow()
-                    food.randomize_pos()
-
-    # move snake
-    snake.move()
+if __name__ == "__main__":
+    pygame.init()
     
-    # snake collisions
-    snake.check_collisions(food)
-
-    # render
-    display.fill("white")
+    g = Game()
+    g.loop()
     
-    food.draw(display)
-    snake.draw(display)
-
-    pygame.display.flip()
-    clock.tick(15)
-
-pygame.quit()
+    pygame.quit()
