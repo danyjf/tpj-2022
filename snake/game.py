1b11fead96f969e4f9d0d72327259b6935adb2f6
Daniel Francisco <deny.jo@hotmail.com>
2022-10-17 12:16:08 +0100

    Refactor game to use snake list

--- snake/game.py
+++ snake/game.py
@@ -10,31 +10,31 @@ class Game(Observer):
     def __init__(self):
         self.display = pygame.display.set_mode((Environment.SCALE * Environment.WIDTH, Environment.SCALE * Environment.HEIGHT))
         self.clock = pygame.time.Clock()
-        self.snake = Snake(40, 10)
-        self.snake.add_observer(self)
-        self.snake2 = Snake(40, 30)
-        self.snake2.add_observer(self)
+        # self.snakes = [Snake(40, 10), Snake(40, 30)]
+        self.snakes = [Snake(40, 10)]
+        for snake in self.snakes:
+            snake.add_observer(self)
         self.food = Food()
         self.input_handler = InputHandler()
         self.running = True
     
     def get_input(self, event):
         if event.key == pygame.K_w:
-            self.input_handler.handle_input('w', self.snake)
+            self.input_handler.handle_input('w', self.snakes[0])
         elif event.key == pygame.K_s:
-            self.input_handler.handle_input('s', self.snake)
+            self.input_handler.handle_input('s', self.snakes[0])
         elif event.key == pygame.K_a:
-            self.input_handler.handle_input('a', self.snake)
+            self.input_handler.handle_input('a', self.snakes[0])
         elif event.key == pygame.K_d:
-            self.input_handler.handle_input('d', self.snake)
+            self.input_handler.handle_input('d', self.snakes[0])
         elif event.key == pygame.K_UP:
-            self.input_handler.handle_input('up', self.snake2)
+            self.input_handler.handle_input('up', self.snakes[-1])
         elif event.key == pygame.K_DOWN:
-            self.input_handler.handle_input('down', self.snake2)
+            self.input_handler.handle_input('down', self.snakes[-1])
         elif event.key == pygame.K_LEFT:
-            self.input_handler.handle_input('left', self.snake2)
+            self.input_handler.handle_input('left', self.snakes[-1])
         elif event.key == pygame.K_RIGHT:
-            self.input_handler.handle_input('right', self.snake2)
+            self.input_handler.handle_input('right', self.snakes[-1])
     
     def on_notify(self, entity, event):
         if event == Environment.EAT:
@@ -51,20 +51,17 @@ class Game(Observer):
                 elif event.type == pygame.KEYDOWN:
                     self.get_input(event)
 
-            # move snake
-            self.snake.move()
-            self.snake2.move()
-            
-            # snake collisions
-            self.snake.check_collisions(self.food)
-            self.snake2.check_collisions(self.food)
+            # move snake and check collisions
+            for snake in self.snakes:
+                snake.move()
+                snake.check_collisions(self.food)
 
             # render
             self.display.fill("white")
             
             self.food.draw(self.display)
-            self.snake.draw(self.display)
-            self.snake2.draw(self.display)
+            for snake in self.snakes:
+                snake.draw(self.display)
 
             pygame.display.flip()
             self.clock.tick(15)
